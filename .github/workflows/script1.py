470bb9948f2f6aa2caba82daebffac0d64458fe4
unanottecosi <30680914+unanottecosi@users.noreply.github.com>
2023-12-18 22:50:52 -0800

    Update script1.py

--- .github/workflows/script1.py
+++ .github/workflows/script1.py
@@ -1,6 +1,11 @@
+import os
 
 def main():
   print("Hello from GitHub Actions!")
+  token = os.environ.get("DAN_SECRET_TOKEN")
+  if not token:
+    raise RuntimeError("DAN_SECRET_TOKEN env var is not set!")
+  print("All good! I found my env var")
 
 if __name__ == '__main__':
   main()
